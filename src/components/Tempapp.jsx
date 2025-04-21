c5d52847746e4f717ffbe73d2059c90f953dadd7
AkashThakur14 <ak9988385532@gmail.com>
2025-04-21 12:30:00 +0530

    initial commit

---
+++ src/components/Tempapp.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+
+export const Tempapp = () => {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("Pathankot");
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=b4ac073fba150e654cb1f77a10ff52c7`;
+      const response = await fetch(url);
+      const resJson = await response.json();
+      
+      // Check if the response is valid
+      if (resJson.main) {
+        setCity(resJson.main);
+      } else {
+        setCity(null);
+      }
+    };
+
+    fetchApi();
+  }, [search]);
+
+  return (
+    <>
+      <div className="box">
+        <div className="inputData">
+          <input
+            type="search"
+            className="inputField"
+            placeholder="Search..."
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
+        </div>
+
+        {!city ? (
+          <p>No Data Found</p>
+        ) : (
+          <div className="information">
+            <h1 className="location">
+              <i className="fa-solid fa-street-view"></i> {search}
+            </h1>
+            <h2 className="temp">{(city.temp - 273.15).toFixed(2)}°C</h2>
+            <h3 className="tempminmax">
+              Min: {(city.temp_min - 273.15).toFixed(2)}°C | Max: {(city.temp_max - 273.15).toFixed(2)}°C
+            </h3>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
